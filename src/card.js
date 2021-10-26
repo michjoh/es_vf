f2b79374e201eacbae3e7eb68164ded474a2f0f1
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-26 15:37:54 +0200

    testable time

--- src/card.js
+++ src/card.js
@@ -1,48 +1,50 @@
-module.exports = function card(card_id) {
-    let events = [];
-    let limit;
-    let used = 0;
+module.exports = function cardModule(now) {
+    return function card(card_id) {
+        let events = [];
+        let limit;
+        let used = 0;
 
-    // invariant
-    function limitAlreadyAssigned() {
-        return limit != null;
+        // invariant
+        function limitAlreadyAssigned() {
+            return limit != null;
+        }
+
+        function notEnoughMoney(amount) {
+            return amount > availableLimit();
+        }
+
+        function availableLimit() {
+            return limit - used;
+        }
+
+        return {
+            assignLimit(amount) {
+                // business invariant/rule
+                if(limitAlreadyAssigned()) {
+                    throw new Error('Cannot assign limit for the second time');
+                }
+                events.push({type: 'LIMIT_ASSIGNED', amount, card_id, date: now().toJSON()});
+                limit = amount;
+            },
+            availableLimit,
+            withdraw(amount) {
+                if(!limitAlreadyAssigned()) {
+                    throw new Error('No limit assigned');
+                }
+                if (notEnoughMoney(amount)) {
+                    throw new Error('Not enough money');
+                }
+                events.push({type: 'CARD_WITHDRAWN', amount, card_id, date: now().toJSON()});
+                used += amount;
+            },
+            repay(amount) {
+                events.push({type: 'CARD_REPAID', amount, card_id, date: now().toJSON()});
+                used -= amount;
+            },
+            pendingEvents() {
+                return events;
+            },
+            uuid() { return card_id; },
+        };
     }
-
-    function notEnoughMoney(amount) {
-        return amount > availableLimit();
-    }
-
-    function availableLimit() {
-        return limit - used;
-    }
-
-    return {
-        assignLimit(amount) {
-            // business invariant/rule
-            if(limitAlreadyAssigned()) {
-                throw new Error('Cannot assign limit for the second time');
-            }
-            events.push({type: 'LIMIT_ASSIGNED', amount, card_id});
-            limit = amount;
-        },
-        availableLimit,
-        withdraw(amount) {
-            if(!limitAlreadyAssigned()) {
-                throw new Error('No limit assigned');
-            }
-            if (notEnoughMoney(amount)) {
-                throw new Error('Not enough money');
-            }
-            events.push({type: 'CARD_WITHDRAWN', amount, card_id});
-            used += amount;
-        },
-        repay(amount) {
-            events.push({type: 'CARD_REPAID', amount, card_id});
-            used -= amount;
-        },
-        pendingEvents() {
-            return events;
-        },
-        uuid() { return card_id; },
-    };
-}
+};
