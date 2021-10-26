86bc664e92353195cc35f2636a6f4e197f03aa13
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-26 15:46:54 +0200

    reduce

--- src/card.js
+++ src/card.js
@@ -1,5 +1,5 @@
 module.exports = function cardModule(now) {
-    return function card(card_id) {
+    function card(card_id) {
         let events = [];
         let limit;
         let used = 0;
@@ -17,10 +17,23 @@ module.exports = function cardModule(now) {
             return limit - used;
         }
 
+        function apply(event) {
+            if (event.type === 'LIMIT_ASSIGNED') {
+                limit = event.amount;
+            }
+            if (event.type === 'CARD_WITHDRAWN') {
+                used += event.amount;
+            }
+            if (event.type === 'CARD_REPAID') {
+                used -= event.amount;
+            }
+        }
+
         return {
+            apply,
             assignLimit(amount) {
                 // business invariant/rule
-                if(limitAlreadyAssigned()) {
+                if (limitAlreadyAssigned()) {
                     throw new Error('Cannot assign limit for the second time');
                 }
                 events.push({type: 'LIMIT_ASSIGNED', amount, card_id, date: now().toJSON()});
@@ -28,7 +41,7 @@ module.exports = function cardModule(now) {
             },
             availableLimit,
             withdraw(amount) {
-                if(!limitAlreadyAssigned()) {
+                if (!limitAlreadyAssigned()) {
                     throw new Error('No limit assigned');
                 }
                 if (notEnoughMoney(amount)) {
@@ -44,7 +57,18 @@ module.exports = function cardModule(now) {
             pendingEvents() {
                 return events;
             },
-            uuid() { return card_id; },
+            uuid() {
+                return card_id;
+            },
         };
     }
+
+    function recreateFrom(card_id, events) {
+        return events.reduce((card, event) => {
+            card.apply(event);
+            return card;
+        }, card(card_id));
+    }
+
+    return {card, recreateFrom};
 };
