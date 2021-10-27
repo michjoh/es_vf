e24eb63880375334820acfcb12a0ec860a84a5c3
kwasniew <kwasniewski.mateusz@gmail.com>
2021-10-27 09:37:29 +0200

    one repo to rule them all

--- src/es.js
+++ src/es.js
@@ -1,22 +1,22 @@
-function initStore() {
-    const es = require('eventstore')({
-        type: 'mongodb',
-        host: 'localhost',
-        port: 27017,
-        dbName: 'card_eventstore_new',
-        eventsCollectionName: 'events',
-        snapshotsCollectionName: 'snapshots',
-        transactionsCollectionName: 'transactions',
-        timeout: 10000,
-        options: {
-            useNewUrlParser: true
-        }
-    });
+function initStore(config = {
+    type: 'mongodb',
+    host: 'localhost',
+    port: 27017,
+    dbName: 'card_eventstore_new',
+    eventsCollectionName: 'events',
+    snapshotsCollectionName: 'snapshots',
+    transactionsCollectionName: 'transactions',
+    timeout: 10000,
+    options: {
+        useNewUrlParser: true
+    }
+}) {
+    const es = require('eventstore')(config);
     //  declaration merging
 
     // ACL - anti corruption layer
     es.close = function() {
-        return es.store.db.close();
+        return es.store.db && es.store.db.close();
     }
 
     return new Promise(function(resolve, reject) {
